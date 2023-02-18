17ec43f942b3c0787537708deaf1de1ad3070ef8
Brian Rahadi <brian.rahadi@gmail.com>
2023-02-18 12:52:55 -0800

    checkpoint

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -20,20 +20,48 @@ const Login: React.FC<LoginProps> = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <Input required name="email" placeholder="email" mb={2} onChange={onChange} />
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        mb={2}
+        onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+      />
       <Input
         name="password"
         placeholder="password"
         mb={2}
         onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
       />
-      <Button
-        width="100%"
-        height="36px"
-        mt={2}
-        mb={2}
-        type="submit"
-       >
+      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
         Login
       </Button>
     </form>
