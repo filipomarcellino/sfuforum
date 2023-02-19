744d667e6863f2dbcc5cfdc1ac8a3069e0e9f660
Filipo Marcellino <filipomarcellino@d207-023-173-096.wireless.sfu.ca>
2023-02-18 18:19:03 -0800

    added communities and community modal

---
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -0,0 +1,88 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Box,
+  Divider,
+  Text,
+  Input
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+type CreateCommunityModalProps = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
+  open,
+  handleClose
+}) => {
+  const [communityName, setCommunityName] = useState("");
+  const [charsRemaning, setCharsRemaining] = useState(21);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > 21) {
+      return;
+    }
+    setCommunityName(event.target.value);
+    setCharsRemaining(21 - event.target.value.length);
+  };
+  return (
+    <>
+      <Modal isOpen={open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            display="flex"
+            flexDirection="column"
+            fontSize={15}
+            padding={3}
+          >
+            Create a community
+          </ModalHeader>
+          <Box pl={3} pr={3}>
+            <Divider />
+            <ModalCloseButton />
+            <ModalBody display="flex" flexDirection="column" padding="10px 0px">
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color="gray.500">
+                Community names including capitalization cannot be changed
+              </Text>
+              <Text
+                position="relative"
+                top="20px"
+                left="10px"
+                width="20px"
+                color="gray.400"
+              >
+                r/
+              </Text>
+              <Input
+                position="relative"
+                value={communityName}
+                size="sm"
+                pl="22px"
+                onChange={() => {}}
+              ></Input>
+              <Text>{21}Characters remaining</Text>
+            </ModalBody>
+          </Box>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Create Community</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default CreateCommunityModal;
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -4,17 +4,18 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
-  InputRightElement,
+  InputRightElement
 } from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
 
-// type SearchInputProps = {
+type SearchInputProps = {
+  user?: User | null;
+};
 
-// };
-
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} align="center">
+    <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
       <InputGroup>
         <InputLeftElement
           pointerEvents="none"
@@ -25,17 +26,17 @@ const SearchInput: React.FC<SearchInputProps> = () => {
           placeholder="search sfure"
           fontSize="10pt"
           _placeholder={{
-            color: "gray.500",
+            color: "gray.500"
           }}
           _hover={{
             bg: "white",
             border: "1px solid",
-            borderColor: "blue.500",
+            borderColor: "blue.500"
           }}
           _focus={{
             outline: "none",
             border: "1px solid",
-            borderColor: "blue.500",
+            borderColor: "blue.500"
           }}
           height="34px"
           bg="gray.50"
