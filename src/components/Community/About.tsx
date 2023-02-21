cb5f8ca6b025c0ebe82f84b1737584a5eff41650
Filipo Marcellino <fmarcellino7@gmail.com>
2023-02-21 14:42:16 -0800

    add about community component(1/2)

--- src/components/Community/About.tsx
+++ src/components/Community/About.tsx
@@ -1,13 +1,31 @@
 import { Community } from "@/src/atoms/communitiesAtom";
-import { Box, Flex, Icon, Text } from "@chakra-ui/react";
-import React from "react";
+import { auth } from "@/src/firebase/clientApps";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Icon,
+  Stack,
+  Text
+} from "@chakra-ui/react";
+import moment from "moment";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useRef } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { RiCakeLine } from "react-icons/ri";
 
 type AboutProps = {
   communityData: Community;
 };
 
 const About: React.FC<AboutProps> = ({ communityData }) => {
+  const router = useRouter();
+  const [user] = useAuthState(auth);
+  const selectedFieldRef = useRef<HTMLInputElement>(null);
+  console.log(router.query);
   return (
     <Box position="sticky" top="14px">
       <Flex
@@ -22,6 +40,64 @@ const About: React.FC<AboutProps> = ({ communityData }) => {
           About Community
         </Text>
         <Icon as={HiOutlineDotsHorizontal} cursor="pointer" />
+      </Flex>
+      <Flex direction="column" p={3} bg="white" borderRadius="0px 0px 4px 4px">
+        <Stack>
+          <Flex width="100%" p={2} fontSize="10pt" fontWeight={700}>
+            <Flex direction="column" flexGrow={1}>
+              <Text>{communityData.numberOfMembers.toLocaleString()}</Text>
+              <Text>Members</Text>
+            </Flex>
+            <Flex direction="column" flexGrow={1}>
+              <Text>1</Text>
+              <Text>Online</Text>
+            </Flex>
+          </Flex>
+          <Divider />
+          <Flex
+            align="center"
+            width="100%"
+            p={1}
+            fontWeight={500}
+            fontSize="10pt"
+          >
+            <Icon as={RiCakeLine} fontSize={18} mr={2} />
+            {communityData.createdAt && (
+              <>
+                <Text>
+                  Created{" "}
+                  {moment(
+                    new Date(communityData.createdAt.seconds * 1000)
+                  ).format("MMM DD, YYYY")}
+                </Text>
+              </>
+            )}
+          </Flex>
+          <Flex>
+            <Button width={"100%"} height="30px">
+              <Link href={`/r/${router.query.communityId}/submit`}>
+                Create Post
+              </Link>
+            </Button>
+          </Flex>
+          {user?.uid === communityData.creatorId && (
+            <>
+              <Divider />
+              <Stack spacing={1} fontSize="10pt">
+                <Flex align="center" justify="space-between">
+                  <Text
+                    color="blue.500"
+                    cursor="pointer"
+                    _hover={{ textDecoration: "underline" }}
+                    onClick={() => selectedFieldRef.current?.click()}
+                  >
+                    Change Image
+                  </Text>
+                </Flex>
+              </Stack>
+            </>
+          )}
+        </Stack>
       </Flex>
     </Box>
   );
--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,5 +1,6 @@
 import { Post } from "@/src/atoms/postAtom";
 import { firestore, storage } from "@/src/firebase/clientApps";
+import useSelectFile from "@/src/hooks/useSelectFile";
 import { Alert, AlertIcon, Flex, Icon, Text } from "@chakra-ui/react";
 import { User } from "firebase/auth";
 import {
@@ -54,7 +55,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     title: "",
     body: ""
   });
-  const [selectedFile, setSelectedFile] = useState<string>();
+  const {onSelectFile, selectedFile, setSelectedFile} = useSelectFile();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -102,18 +103,6 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
     setLoading(false);
   };
 
-  const onSelectedImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    if (event.target.files?.[0]) {
-      reader.readAsDataURL(event.target.files[0]);
-    }
-    reader.onload = (readerEvent) => {
-      if (readerEvent.target?.result) {
-        setSelectedFile(readerEvent.target.result as string);
-      }
-    };
-  };
-
   const onTextChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -150,7 +139,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
         {selectedTab === "Images & Video" && (
           <ImageUpload
             selectedFile={selectedFile}
-            onSelectedImage={onSelectedImage}
+            onSelectedImage={onSelectFile}
             setSelectTab={setSelectedTab}
             setSelectedFile={setSelectedFile}
           />
