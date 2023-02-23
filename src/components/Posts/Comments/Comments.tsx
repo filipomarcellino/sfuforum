35328f1af5c778a88f73e8a2b5552d01612ea5ca
Filipo Marcellino <fmarcellino7@gmail.com>
2023-02-23 11:03:26 -0800

    add comment section

---
+++ src/components/Posts/Comments/Comments.tsx
@@ -0,0 +1,52 @@
+import { Post } from "@/src/atoms/postAtom";
+import { Box, Flex } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import CommentInput from "./CommentInput";
+
+type CommentsProps = {
+  user: User;
+  selectedPost: Post | null;
+  communityId: string;
+};
+
+const Comments: React.FC<CommentsProps> = ({
+  user,
+  selectedPost,
+  communityId
+}) => {
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState([]);
+  const [fetchLoading, setFetchLoading] = useState(true);
+  const [createLoading, setCreateLoading] = useState(false);
+  const onCreateComment = async (commentText: string) => {};
+  const onDeleteComment = async (comment: any) => {};
+  const getPostComments = async () => {};
+
+  useEffect(() => {
+    // if (!selectedPost) return;
+    getPostComments();
+  }, []);
+
+  return (
+    <Box bg={"white"} borderRadius="0px 0px 4px 4px" p={2}>
+      <Flex
+        direction="column"
+        pl={10}
+        pr={4}
+        mb={6}
+        fontSize="10pt"
+        width="100%"
+      >
+        <CommentInput
+          commentText={commentText}
+          setCommentText={setCommentText}
+          user={user}
+          createLoading={createLoading}
+          onCreateComment={onCreateComment}
+        />
+      </Flex>
+    </Box>
+  );
+};
+export default Comments;
